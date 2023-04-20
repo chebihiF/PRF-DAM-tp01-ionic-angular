59e081249af51822549889166522913d8d92867b
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-04-20 14:32:26 +0000

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RecipesPage } from './recipes/recipes.page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: RecipesPage
+  },
+];
