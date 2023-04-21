ec75a968b687c9c91b6f978a312e1cdaa21edeab
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-04-21 09:05:02 +0000

    setup navigations

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class RecipesPage implements OnInit {
 
